874830b8523b0b4a01b50c1a22b994e3f1c53fde
agentraghav <raghvendrarathore2@gmail.com>
2021-03-02 12:48:42 +0530

    post model created

---
+++ models/posts.js
@@ -0,0 +1,16 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const postSchema = new Schema(
+  {
+    content: { type: String },
+    image: { type: Object },
+    user: { type: Schema.Types.ObjectId, ref: 'User' },
+    likes: [{ type: Schema.Types.ObjectId, ref: 'User' }],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model('Post', postSchema);
